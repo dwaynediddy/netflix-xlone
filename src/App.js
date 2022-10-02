66ef23cd424d29aaa3d20defd700cf6fd0d2d41e
dwaynediddy <burrows.dwayne@icloud.com>
2022-10-03 00:50:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Counter } from './features/counter'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Netflix-xlone</h2>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDIkBxKlmg7kEZIKFy10AbPvWmOYDjaQPw",
+    authDomain: "netflix-xclone.firebaseapp.com",
+    projectId: "netflix-xclone",
+    storageBucket: "netflix-xclone.appspot.com",
+    messagingSenderId: "942586859604",
+    appId: "1:942586859604:web:f27e34c57126a100f5ddc1"
+  };
