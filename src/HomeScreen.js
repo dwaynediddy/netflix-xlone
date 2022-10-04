85ad8f2c5741a2ce5d1ba18e1a48a96e2aa2bd76
dwaynediddy <burrows.dwayne@icloud.com>
2022-10-05 00:42:18 +0200

    added rows and fetched from api

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Banner from './components/Banner'
 import Nav from './components/Nav'
+import Row from './components/Row'
+import requests from './Request'
 import './styles/HomeScreen.css'
 
 const HomeScreen = () => {
@@ -8,8 +10,39 @@ const HomeScreen = () => {
     <div className="homeScreen">
        <Nav />
        <Banner />
-
-        {/* row */}
+       <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+       />
+       <Row
+        title="Trending Now"
+        fetchUrl={requests.fetchTrendings}
+       />
+       <Row
+        title="Top Rated"
+        fetchUrl={requests.fetchTopRated}
+       />
+       <Row
+        title="Action Movie"
+        fetchUrl={requests.fetchActionMovies}
+       />
+       <Row
+        title="Comedy Movie"
+        fetchUrl={requests.fetchComedeyMovies}
+       />
+       <Row
+        title="Horror Movie"
+        fetchUrl={requests.fetchHorrorMovies}
+       />
+       <Row
+        title="Romance Movie"
+        fetchUrl={requests.fetchRomanceMovies}
+       />
+       <Row
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+       />
     </div>
   )
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -29,26 +29,17 @@ const Banner = () => {
   return (
     <header className="banner" style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
     }}>
       <div className="banner-contents">
-        <h1 className="banner-title">
-          Movie Name
-        </h1>
+        <h1 className="banner-title">{movie?.title || movie?.name || movie?.original_name}</h1>
         <div className="banner-buttons">
           <button className='banner-button'>Play</button>
           <button className='banner-button'>My List</button>
         </div>
         <h1 className="banner-description">
-          {truncate(
-            `banner description test banner description testbanner description test
-            banner description testbanner description testbanner description testbanner description test
-            banner description testbanner description testbanner description testbanner description test
-            banner description testbanner description testbanner description testbanner description test
-            banner description testbanner description testbanner description testbanner description test
-            banner description testbanner description testbanner description testbanner description test
-            banner description testbanner description testbanner description testbanner description test`, 150
+          {truncate(`${movie?.overview}`, 150
             )}
           </h1>
       </div>
